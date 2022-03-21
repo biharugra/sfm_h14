2b9d33fb59380c3ccbfe45a32008ddf6d0f4eb05
Kristof0822 <45668395+Kristof0822@users.noreply.github.com>
2022-03-21 20:43:52 +0100

    Add files via upload
    python fájl

---
+++ 2022.py
@@ -0,0 +1,12 @@
+#A program lényege, hogy a 2022 számot úgy írjuk ki a képernyőre, hogy nem használunk egyetlen számot sem.
+
+def main():
+
+    num1 = 'x'
+    num2 = 'd'
+    num3 = 'b'
+    print(ord(num1) - ord(num2), end = "")
+    print(ord(num1) - ord(num3))
+
+if __name__ == "__main__":
+    main()
